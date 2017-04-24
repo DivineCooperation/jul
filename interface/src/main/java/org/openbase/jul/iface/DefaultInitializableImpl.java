201c894e1d09bba1f1bc8f8d5a30939ab554b6c3
Julian Daberkow <jdaberkow@techfak.uni-bielefeld.de>
2017-04-24 17:03:06 +0200

    Remove default methods in DefaultInitializableImpl interface

--- interface/src/main/java/org/openbase/jul/iface/DefaultInitializableImpl.java
+++ interface/src/main/java/org/openbase/jul/iface/DefaultInitializableImpl.java
@@ -33,11 +33,12 @@ import org.openbase.jul.iface.provider.DefaultConfigProvider;
 public interface DefaultInitializableImpl<CONFIG> extends Initializable<CONFIG>, DefaultInitializable, DefaultConfigProvider<CONFIG> {
 
     @Override
-    public default void init() throws InitializationException, InterruptedException {
-        try {
-            init(getDefaultConfig());
-        } catch (CouldNotPerformException ex) {
-            throw new InitializationException(this, ex);
-        }
-    }
+    public void init() throws InitializationException, InterruptedException;
+//    public default void init() throws InitializationException, InterruptedException {
+//        try {
+//            init(getDefaultConfig());
+//        } catch (CouldNotPerformException ex) {
+//            throw new InitializationException(this, ex);
+//        }
+//    }
 }
